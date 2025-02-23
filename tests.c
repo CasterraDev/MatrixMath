4e82e1994b02e2cbd3ff25e84deb06e9f982eebd
CasterraDev <casterradev@gmail.com>
2025-02-23 15:14:39 -0500

    Added: Tests for submatrix

--- tests.c
+++ tests.c
@@ -6,19 +6,23 @@
 
 #define shift(xs, xs_sz) (assert(xs_sz > 0), --xs_sz, *xs++)
 
-int testTranspose(void){
+void fillMatrixByCounting(Matrix* m) {
+    double c = 0;
+    for (int i = 0; i < m->rows; i++) {
+        for (int j = 0; j < m->cols; j++) {
+            c++;
+            MAT_AT(m, i, j) = c;
+        }
+    }
+}
+
+int testTranspose(void) {
     Matrix* mat = matrixCreate(3, 3);
     Matrix* mat2 = matrixCreate(3, 3);
-    MAT_AT(mat, 0, 0) = 1;
-    MAT_AT(mat, 0, 1) = 2;
-    MAT_AT(mat, 0, 2) = 3;
-    MAT_AT(mat, 1, 0) = 4;
-    MAT_AT(mat, 1, 1) = 5;
-    MAT_AT(mat, 1, 2) = 6;
-    MAT_AT(mat, 2, 0) = 7;
-    MAT_AT(mat, 2, 1) = 8;
-    MAT_AT(mat, 2, 2) = 9;
+    fillMatrixByCounting(mat);
+    matrixPrint(mat, "Transpose Test", 0);
     matrixTranspose(mat2, mat);
+    matrixPrint(mat2, "Transpose Result Matrix", 0);
     assert(MAT_AT(mat2, 0, 0) == 1);
     assert(MAT_AT(mat2, 0, 1) == 4);
     assert(MAT_AT(mat2, 0, 2) == 7);
@@ -28,12 +32,30 @@ int testTranspose(void){
     assert(MAT_AT(mat2, 2, 0) == 3);
     assert(MAT_AT(mat2, 2, 1) == 6);
     assert(MAT_AT(mat2, 2, 2) == 9);
+    printf("Test Passed\n");
+    return 0;
+}
+
+int testSubmat(void) {
+    Matrix* mat = matrixCreate(3, 3);
+    fillMatrixByCounting(mat);
+    matrixPrint(mat, "Submatrix Test", 0);
+    int ra[] = {1};
+    int ca[] = {1};
+    Matrix* dest = matrixSubmatrixCreate(mat, ra, 1, ca, 1);
+    matrixPrint(dest, "Submatrix Result Matrix", 0);
+    assert(MAT_AT(dest, 0, 0) == 1);
+    assert(MAT_AT(dest, 0, 1) == 3);
+    assert(MAT_AT(dest, 1, 0) == 7);
+    assert(MAT_AT(dest, 1, 1) == 9);
+    printf("Test Passed\n");
     return 0;
 }
 
 int main(void) {
     printf("Starting Tests...\n");
     testTranspose();
+    testSubmat();
     printf("Finished Tests\n");
     return 0;
 }
